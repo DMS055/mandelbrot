74c8329856ac5d593dd209a93b7f8d63b66c69b1
passagenoir <60157796+DMS055@users.noreply.github.com>
2025-07-13 02:44:39 +0200

    Added drawing finction

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 document.addEventListener("DOMContentLoaded", setup);
 
+const pixelSize = 10;
+let canvasWidth;
+let canvasHeight;
+
 function setup() {
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute("width", 400);
-    canvas.setAttribute("height", 400);
-    canvas.setAttribute("class", "mandelbrot-canvas");
-    document.querySelector(".mandelbrot-canvas-container").appendChild(canvas);
+	const canvas = document.createElement("canvas");
+	canvas.setAttribute("width", 400);
+	canvas.setAttribute("height", 400);
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
+	canvas.setAttribute("class", "mandelbrot-canvas");
+	document.querySelector(".mandelbrot-canvas-container").appendChild(canvas);
+	drawImage();
+}
+
+function drawImage() {
+    const canvas = document.querySelector(".mandelbrot-canvas");
+    
+    let xColumn;
+    let yColumn;
+
+	let ctx = canvas.getContext("2d");
+	let pixel = ctx.getImageData(0, 0, 10, 10);
+
+
+    for (yColumn = 0; yColumn < canvasHeight; yColumn += pixelSize) {
+        for (xColumn = 0; xColumn < canvasWidth; xColumn += pixelSize) {
+            colorPixel();
+            ctx.putImageData(pixel, xColumn, yColumn);
+        }
+    }
+
+    function colorPixel() {
+			for (let i = 0; i < pixel.data.length; i += 4) {
+				pixel.data[i] = yColumn; // Red
+				pixel.data[i + 1] = 0; // Green
+				pixel.data[i + 2] = 0; // Blue
+				pixel.data[i + 3] = 255; // Alpha
+			}
+		}
 }
