923284042c1b1822171bc944e0c18d1cd540cf26
passagenoir <60157796+DMS055@users.noreply.github.com>
2025-07-14 08:22:45 +0200

    Added Coordinate class

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ const pixelSize = 10;
 let canvasWidth;
 let canvasHeight;
 
+class Coordinate {
+
+	constructor(x, i) {
+		this.x = x;
+		this.i = i;
+	}
+
+	squareCoordinate() {
+		return new Coordinate(this.x * this.x - (this.i * this.i), 2 * (this.x * this.i));
+	}
+
+	addCoordinate(otherCoordinate) {
+		return new Coordinate(this.x + otherCoordinate.x, this.i + otherCoordinate.i);
+	}
+
+	toString() {
+		return `(${this.x}, ${this.i}i)`;
+	}
+}
+
 function setup() {
 	const canvas = document.createElement("canvas");
 	canvas.setAttribute("width", 400);
 	canvas.setAttribute("height", 400);
 	canvasWidth = canvas.width;
 	canvasHeight = canvas.height;
+
 	canvas.setAttribute("class", "mandelbrot-canvas");
 	document.querySelector(".mandelbrot-canvas-container").appendChild(canvas);
+
+	let testC = new Coordinate(1, -2);
+
+	console.log(testC.squareCoordinate().toString()); 
 	drawImage();
-}
+}	
 
 function drawImage() {
     const canvas = document.querySelector(".mandelbrot-canvas");
