84dbf44c90f8fbdcb4bad0fb97b21fc1a7b7d950
passagenoir <60157796+DMS055@users.noreply.github.com>
2025-09-07 00:49:53 +0200

    Added functions for calculating z and determining the color of a point

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ document.addEventListener("DOMContentLoaded", setup);
 const pixelSize = 10;
 let canvasWidth;
 let canvasHeight;
+maxIterations = 60;
 
 class Coordinate {
-
 	constructor(x, i) {
 		this.x = x;
 		this.i = i;
 	}
 
 	squareCoordinate() {
-		return new Coordinate(this.x * this.x - (this.i * this.i), 2 * (this.x * this.i));
+		return new Coordinate(
+			this.x * this.x - this.i * this.i,
+			2 * (this.x * this.i)
+		);
 	}
 
 	addCoordinate(otherCoordinate) {
-		return new Coordinate(this.x + otherCoordinate.x, this.i + otherCoordinate.i);
+		return new Coordinate(
+			this.x + otherCoordinate.x,
+			this.i + otherCoordinate.i
+		);
 	}
 
 	toString() {
 		return `(${this.x}, ${this.i}i)`;
+	}
+
+	magnitude() {
+		return Math.sqrt(this.x * this.x + this.i * this.i);
 	}
 }
 
@@ -34,35 +44,59 @@ function setup() {
 	canvas.setAttribute("class", "mandelbrot-canvas");
 	document.querySelector(".mandelbrot-canvas-container").appendChild(canvas);
 
-	let testC = new Coordinate(1, -2);
+	let testC = new Coordinate(-1, 0);
 
-	console.log(testC.squareCoordinate().toString()); 
+	console.log(determineIterations(testC));
 	drawImage();
-}	
+}
+
+function determineIterations(c) {
+	let numIterations = 0;
+
+	let z = c;
+	while (z.magnitude() < 2 && numIterations < maxIterations) {
+		z = z.squareCoordinate().addCoordinate(c);
+		numIterations++;
+	}
+	return numIterations;
+}
+
+function determineColor(numIterations) {
+	if (numIterations === maxIterations) {
+		return { r: 0, g: 0, b: 0 }; // Black if inside the set
+	} else {
+		return {
+			r: (numIterations / maxIterations) * 255,
+			g: (numIterations / maxIterations) * 255,
+			b: (numIterations / maxIterations) * 255,
+		};
+		/*const hue = Math.floor(360 * numIterations / maxIterations);
+		return hslToRgb(hue, 100, 50);*/
+	}
+}
 
 function drawImage() {
-    const canvas = document.querySelector(".mandelbrot-canvas");
-    
-    let xColumn;
-    let yColumn;
+	const canvas = document.querySelector(".mandelbrot-canvas");
+
+	let xColumn;
+	let yColumn;
 
 	let ctx = canvas.getContext("2d");
 	let pixel = ctx.getImageData(0, 0, 10, 10);
 
-
-    for (yColumn = 0; yColumn < canvasHeight; yColumn += pixelSize) {
-        for (xColumn = 0; xColumn < canvasWidth; xColumn += pixelSize) {
-            colorPixel();
-            ctx.putImageData(pixel, xColumn, yColumn);
-        }
-    }
-
-    function colorPixel() {
-			for (let i = 0; i < pixel.data.length; i += 4) {
-				pixel.data[i] = yColumn; // Red
-				pixel.data[i + 1] = 0; // Green
-				pixel.data[i + 2] = 0; // Blue
-				pixel.data[i + 3] = 255; // Alpha
-			}
+	for (yColumn = 0; yColumn < canvasHeight; yColumn += pixelSize) {
+		for (xColumn = 0; xColumn < canvasWidth; xColumn += pixelSize) {
+			colorPixel();
+			ctx.putImageData(pixel, xColumn, yColumn);
 		}
+	}
+
+	function colorPixel() {
+		for (let i = 0; i < pixel.data.length; i += 4) {
+			pixel.data[i] = xColumn; // Red
+			pixel.data[i + 1] = yColumn; // Green
+			pixel.data[i + 2] = 0; // Blue
+			pixel.data[i + 3] = 255; // Alpha
+		}
+	}
 }
